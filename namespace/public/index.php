df565e5116ce55d55a5acebe0c7d2749f6ed4417
psgangadharan <prem.s.gangadharan@eqs.com>
2020-10-06 11:11:41 +0530

    try

--- namespace/public/index.php
+++ namespace/public/index.php
@@ -4,21 +4,26 @@ require __DIR__.'/../vendor/autoload.php';
 use App\Format;
 use App\Format\JSON;
 use App\Format as F;
-
+/**namespace override*/
 use Guzzle\Http\Client;
+
 use Guzzle\Http\Url;
 use Guzzle\Http\EntityBody;
+
+//use  App\Functions;
 $data=[
     'name'=>'john',
     'place'=>'Munich'
 ];
-/*$json = new JSON($data);
+$json = new JSON($data);
 $yml = new Format\YAML($data);
 $xml = new F\XML($data);
-var_dump($json->convert());
+/*var_dump($json->convert());
 var_dump($yml->convert());
 var_dump($xml->convert());*/
 
+fileopen();
+/**namespace override*/
 $client = new Client();
 echo 'output->'.$client->getBaseUrl();
 
---
+++ namespace/src/Functions/Fileopen.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Functions;
+
+/* This function is A\B\C\fopen */
+function fileopen() {
+    /* ... */
+    echo 'im fopen function';
+}
